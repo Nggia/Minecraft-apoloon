2bfba138f1ffe770517a5b3ff4f1fc55d3262241
Nggia <156265125+Nggia@users.noreply.github.com>
2026-02-15 21:18:37 +0700

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE    := Apollon
 
-# 1. PHẢI THÊM CÁC FILE CPP CỦA THƯ VIỆN VÀO ĐÂY
+# Thêm các file nguồn của thư viện vào đây
 LOCAL_SRC_FILES := main.cpp \
                    ImGui/imgui.cpp \
                    ImGui/imgui_draw.cpp \
@@ -14,13 +14,12 @@ LOCAL_SRC_FILES := main.cpp \
                    ImGui/backends/imgui_impl_opengl3.cpp \
                    KittyMemory/MemoryPatch.cpp \
                    KittyMemory/KittyMemory.cpp \
-                   KittyScanner/KittyScanner.cpp \
-                   Includes/Dobby/dobby.cpp
+                   KittyScanner/KittyScanner.cpp
 
 LOCAL_LDLIBS    := -llog -landroid -lGLESv3 -lEGL -lz
 LOCAL_CPPFLAGS  := -std=c++17 -fexceptions -frtti -Wno-error=format-security
 
-# 2. KHAI BÁO FULL ĐƯỜNG DẪN INCLUDE
+# Khai báo tất cả thư mục chứa file .h từ code main.cpp của bạn
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
                     $(LOCAL_PATH)/ImGui \
                     $(LOCAL_PATH)/Includes \
@@ -28,6 +27,8 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH) \
                     $(LOCAL_PATH)/Images/Platforms \
                     $(LOCAL_PATH)/KittyMemory \
                     $(LOCAL_PATH)/KittyScanner \
-                    $(LOCAL_PATH)/ENCRYPT
+                    $(LOCAL_PATH)/ENCRYPT \
+                    $(LOCAL_PATH)/Arm64 \
+                    $(LOCAL_PATH)/Substrate
 
 include $(BUILD_SHARED_LIBRARY)
