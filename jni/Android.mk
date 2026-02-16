f5262c99986fe98930180a1022fd27b610dce891
Nggia <156265125+Nggia@users.noreply.github.com>
2026-02-16 07:44:16 +0700

    Update Android.mk

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,10 +1,9 @@
 LOCAL_PATH := $(call my-dir)
-
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := Apollon
 
-# Thêm các file nguồn của thư viện vào đây
+# Khai báo các file .cpp chính
 LOCAL_SRC_FILES := main.cpp \
                    ImGui/imgui.cpp \
                    ImGui/imgui_draw.cpp \
@@ -19,15 +18,16 @@ LOCAL_SRC_FILES := main.cpp \
 LOCAL_LDLIBS    := -llog -landroid -lGLESv3 -lEGL -lz
 LOCAL_CPPFLAGS  := -std=c++17 -fexceptions -frtti -Wno-error=format-security
 
-# Khai báo tất cả thư mục chứa file .h từ code main.cpp của bạn
+# Khai báo đường dẫn để GitHub tìm thấy file .h trong các folder bạn đã up
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
                     $(LOCAL_PATH)/ImGui \
                     $(LOCAL_PATH)/Includes \
                     $(LOCAL_PATH)/Images \
-                    $(LOCAL_PATH)/Images/Platforms \
                     $(LOCAL_PATH)/KittyMemory \
                     $(LOCAL_PATH)/KittyScanner \
                     $(LOCAL_PATH)/ENCRYPT \
+                    $(LOCAL_PATH)/ghc \
+                    $(LOCAL_PATH)/glm \
                     $(LOCAL_PATH)/Arm64 \
                     $(LOCAL_PATH)/Substrate
 
